04b96eb1c8dc6acb2e6cd020e8063d008df8dc3f
Иван <ivangl79@gmail.com>
2024-10-15 18:52:51 +0300

    добавлен HTML

--- src/main/java/org/example/springboot_web/controller/HomeController.java
+++ src/main/java/org/example/springboot_web/controller/HomeController.java
@@ -4,14 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-//@Controller
+//@RestController//первый вариант
+@Controller
 public class HomeController {
 
     @RequestMapping("/home")
     public String getHome(@RequestParam(name = "name", required = false, defaultValue = "World")
                           String name, Model model) {
         model.addAttribute("name", name);
-        return "hi " + name;
+        return "home" /*+ name*/;
     }
 }
