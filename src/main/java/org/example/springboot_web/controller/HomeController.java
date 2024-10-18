400dd9b0809b03dbe196dd69ac3c3b449db1a759
Иван <ivangl79@gmail.com>
2024-10-19 01:04:05 +0300

    добавлен поиск и открытие страниц по имени из поиска

--- src/main/java/org/example/springboot_web/controller/HomeController.java
+++ src/main/java/org/example/springboot_web/controller/HomeController.java
@@ -8,10 +8,31 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
-    public String getHome(@RequestParam(name = "name", required = false, defaultValue = "World")
+    @RequestMapping("/search")
+    public String getHome_search(@RequestParam(name = "name", required = false, defaultValue = "search")
                           String name, Model model) {
         model.addAttribute("name", name);
+        return "search";
+    }
+
+    @RequestMapping("/search2")
+    public String getHome_search2(@RequestParam(name = "name", required = false, defaultValue = "search2")
+                                 String name, Model model) {
+        model.addAttribute("name", name);
+        return name;//"name";
+    }
+
+    @RequestMapping("/home")
+    public String getHome_home(@RequestParam(name = "name", required = false, defaultValue = "home")
+                               String name, Model model) {
+        model.addAttribute("name", name);
         return "home";
+    }
+
+    @RequestMapping("/ivan")
+    public String getHome_ivan(@RequestParam(name = "name", required = false, defaultValue = "ivan")
+                          String name, Model model) {
+        model.addAttribute("name", name);
+        return "ivan";
     }
 }
